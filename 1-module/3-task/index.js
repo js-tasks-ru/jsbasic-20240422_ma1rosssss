ed63d59bb3bb1b3423573dfe6c580f4e211c5cde
Vladislav <ma1rosssss@gmail.com>
2024-04-27 20:39:33 +0300

    задача №3

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str.length >= 1) {
+    return str[0].toUpperCase() + str.slice(1);
+  } else {
+    return "";
+  }
+
+  // return str.includes(" ");
 }
+
+console.log(ucFirst(""));
