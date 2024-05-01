eb74abd7edb42c98a7dd6d574bb772167943c39f
Vladislav <ma1rosssss@gmail.com>
2024-05-01 17:51:04 +0300

    решение задачки№1

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,1 +1,20 @@
-function sumSalary(salaries) {}
+let salaries = {
+  John: 1000,
+  Ann: 1600,
+  Pete: 1300,
+  month: "December",
+  currency: "USD",
+  isPayed: false,
+};
+
+function sumSalary(salaries) {
+  let sum = 0;
+  for (let i in salaries) {
+    if (typeof salaries[i] === "number" && isFinite(salaries[i])) {
+      sum += salaries[i];
+    }
+  }
+  return sum;
+}
+
+sumSalary(salaries);
