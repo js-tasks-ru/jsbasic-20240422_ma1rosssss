1368dd8025186e5726222ea4532a09f9b49ede76
Vladislav <ma1rosssss@gmail.com>
2024-05-01 18:03:45 +0300

    решение задачки №2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
+let schedule = {};
+
 function isEmpty(obj) {
-  // ваш код...
+  let arr = Object.keys(obj);
+
+  if (arr.length === 0) {
+    return true;
+  } else if (arr.length > 0) {
+    return false;
+  }
 }
+
+isEmpty(schedule);
