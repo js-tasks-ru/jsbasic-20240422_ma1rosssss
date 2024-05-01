805b0ac6a3952ebe20007fffc6f29a376d0aaf26
Vladislav <ma1rosssss@gmail.com>
2024-05-01 19:10:46 +0300

    решение задачки №3

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,20 @@
 let calculator = {
-  // ваш код
+  read: function (a, b) {
+    this.a = a;
+    this.b = b;
+  },
+
+  sum: function (a, b) {
+    return this.a + this.b;
+  },
+
+  mul: function (a, b) {
+    return this.a * this.b;
+  },
 };
 
+calculator.read(3, 5);
+console.log(calculator.sum()); // 8
+console.log(calculator.mul());
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
