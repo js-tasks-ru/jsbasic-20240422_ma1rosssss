bd27042a9dcaf7ece7b875bd44f4898fbfde10e5
Vladislav <ma1rosssss@gmail.com>
2024-05-04 18:59:40 +0300

    решение задачки №1

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,12 @@
+let vasya = { name: "Вася", age: 25 };
+let petya = { name: "Петя", age: 30 };
+let masha = { name: "Маша", age: 28 };
+
+let users = [vasya, petya, masha];
+
 function namify(users) {
-  // ваш код...
+  return users.map((users) => users.name);
 }
+
+let names = namify(users);
+console.log(names);
