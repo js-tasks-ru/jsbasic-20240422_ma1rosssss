eefc59071c6763b6d2e237342a551c7ddc637b80
Vladislav <ma1rosssss@gmail.com>
2024-05-07 15:39:13 +0300

    решение задачки №4

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,16 @@
+let user1 = {
+  balance: "$1,825.65",
+  picture: "https://placehold.it/32x32",
+  age: 21,
+  name: "Golden Branch",
+  gender: "male",
+  greeting: "Hello, Golden Branch! You have 7 unread messages.",
+  favouriteFruit: "banana",
+};
+
 function showSalary(users, age) {
-  // ваш код...
+  return users
+    .filter((item) => item.age <= age)
+    .map((item) => `${item.name}, ${item.balance}`)
+    .join("\n");
 }
