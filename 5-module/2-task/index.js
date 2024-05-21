8f3eedce918cd43c9de885a434df4de38ced8a8b
Vladislav <ma1rosssss@gmail.com>
2024-05-21 19:17:57 +0300

    решение задачки №2

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,25 @@
+// let button = document.querySelector(".toggle-text-button");
+// document.getElementById("text").setAttribute("hidden", false);
+
+// function toggleText() {
+//   let text = document.getElementById("text");
+
+//   if (text.hasAttribute("hidden")) {
+//     text.removeAttribute("hidden");
+//   } else {
+//     text.setAttribute("hidden", "");
+//   }
+// }
+
+// button.addEventListener("click", toggleText);
+// Подскажите почему код выше прошел авто тест ?
+
 function toggleText() {
-  // ваш код...
+  let toggleButton = document.querySelector(".toggle-text-button");
+  let textElement = document.getElementById("text");
+
+  toggleButton.addEventListener("click", () => {
+    let isTextElementHidden = textElement.hidden;
+    textElement.hidden = !isTextElementHidden;
+  });
 }
